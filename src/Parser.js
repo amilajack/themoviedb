d37a0f59202b5b806be6e38a9d94a656476a17ca
Amila Welihinda <amilajack@gmail.com>
2016-10-29 22:24:06 -0700

    Boilerplate commit

---
+++ src/Parser.js
@@ -0,0 +1,233 @@
+/* eslint newline-per-chained-call: 0 */
+
+/**
+ * Parse all pages
+ */
+import cheerio from 'cheerio';
+import fetch from 'isomorphic-fetch';
+import UrlParse from 'url-parse';
+import { baseUrl } from './Torrent';
+
+
+const maxConcurrentRequests = 3;
+
+export function _parseTorrentIsVIP(element) {
+  return (
+    element.find('img[title="VIP"]').attr('title') === 'VIP'
+  );
+}
+
+export function _parseTorrentIsTrusted(element) {
+  return (
+    element.find('img[title="Trusted"]').attr('title') === 'Trusted'
+  );
+}
+
+export function isTorrentVerified(element) {
+  return _parseTorrentIsVIP(element) || _parseTorrentIsTrusted(element);
+}
+
+export async function getProxyList() {
+  console.log('Retriving proxy list...');
+
+  const response = await fetch('https://proxybay.tv/')
+    .then(res => res.text());
+
+  const $ = cheerio.load(response);
+
+  const links = $('[rel="nofollow"]').map(function getElementLinks() {
+    return $(this).attr('href');
+  })
+  .get()
+  .filter((res, index) => (index < maxConcurrentRequests));
+
+  return links;
+}
+
+export function parsePage(url, parseCallback, filter = {}) {
+  const attempt = async error => {
+    if (error) console.log(error);
+
+    const proxyUrls = [
+      'https://thepiratebay.org',
+      'https://thepiratebay.se',
+      'https://pirateproxy.one',
+      'https://ahoy.one'
+    ];
+
+    const requests = proxyUrls
+      .map(_url => (new UrlParse(url)).set('hostname', new UrlParse(_url).hostname).href)
+      .map(_url => fetch(_url, { mode: 'no-cors' }));
+
+    return Promise.race(requests).then(response => response.text());
+  };
+
+  return attempt()
+    .then(response => (
+      response.includes('Database maintenance')
+        ? (attempt('Failed because of db error, retrying'))
+        : response
+    ))
+    .then(response => parseCallback(response, filter));
+}
+
+export function parseResults(resultsHTML, filter = {}) {
+  const $ = cheerio.load(resultsHTML);
+  const rawResults = $('table#searchResult tr:has(a.detLink)');
+
+  const results = rawResults.map(function getRawResults() {
+    const name = $(this).find('a.detLink').text();
+    const uploadDate = $(this).find('font').text()
+      .match(/Uploaded\s(?:<b>)?(.+?)(?:<\/b>)?,/)[1];
+    const size = $(this).find('font').text()
+      .match(/Size (.+?),/)[1];
+
+    const seeders = $(this).find('td[align="right"]').first().text();
+    const leechers = $(this).find('td[align="right"]').next().text();
+    const relativeLink = $(this).find('div.detName a').attr('href');
+    const link = baseUrl + relativeLink;
+    const id = parseInt(/^\/torrent\/(\d+)/.exec(relativeLink)[1], 10);
+    const magnetLink = $(this).find('a[title="Download this torrent using magnet"]').attr('href');
+    const uploader = $(this).find('font .detDesc').text();
+    const uploaderLink = baseUrl + $(this).find('font a').attr('href');
+    const verified = isTorrentVerified($(this));
+
+    const category = {
+      id: $(this).find('center a').first().attr('href').match(/\/browse\/(\d+)/)[1],
+      name: $(this).find('center a').first().text()
+    };
+
+    const subcategory = {
+      id: $(this).find('center a').last().attr('href').match(/\/browse\/(\d+)/)[1],
+      name: $(this).find('center a').last().text()
+    };
+
+    return {
+      id,
+      name,
+      size,
+      link,
+      category,
+      seeders,
+      leechers,
+      uploadDate,
+      magnetLink,
+      subcategory,
+      uploader,
+      verified,
+      uploaderLink
+    };
+  });
+
+  const parsedResultsArray =
+    results
+      .get()
+      .filter(result => !result.uploaderLink.includes('undefined'));
+
+  if (filter.verified === true) {
+    return parsedResultsArray.filter(result => result.verified === true);
+  }
+
+  return parsedResultsArray;
+}
+
+export function parseTvShow(tvShowPage) {
+  const $ = cheerio.load(tvShowPage);
+
+  const seasons = $('dt a').map(() => $(this).text()).get();
+
+  const rawLinks = $('dd');
+
+  const torrents = rawLinks.map(element =>
+    $(this).find('a').map(() => ({
+      title: element.text(),
+      link: baseUrl + element.attr('href'),
+      id: element.attr('href').match(/\/torrent\/(\d+)/)[1]
+    }))
+    .get()
+  );
+
+  return seasons.map(
+    (season, index) => ({ title: season, torrents: torrents[index] })
+  );
+}
+
+export function parseTorrentPage(torrentPage) {
+  const $ = cheerio.load(torrentPage);
+  const name = $('#title').text().trim();
+
+  const size = $('dt:contains(Size:) + dd').text().trim();
+  const uploadDate = $('dt:contains(Uploaded:) + dd').text().trim();
+  const uploader = $('dt:contains(By:) + dd').text().trim();
+  const uploaderLink = baseUrl + $('dt:contains(By:) + dd a').attr('href');
+  const seeders = $('dt:contains(Seeders:) + dd').text().trim();
+  const leechers = $('dt:contains(Leechers:) + dd').text().trim();
+  const id = $('input[name=id]').attr('value');
+  const link = `${baseUrl}/torrent/${id}`;
+  const magnetLink = $('a[title="Get this torrent"]').attr('href');
+  const description = $('div.nfo').text().trim();
+
+  return {
+    name,
+    size,
+    seeders,
+    leechers,
+    uploadDate,
+    magnetLink,
+    link,
+    id,
+    description,
+    uploader,
+    uploaderLink
+  };
+}
+
+export function parseTvShows(tvShowsPage) {
+  const $ = cheerio.load(tvShowsPage);
+  const rawTitles = $('dt a');
+
+  const series = rawTitles.map(
+    (element) => ({
+      title: element.text(),
+      id: element.attr('href').match(/\/tv\/(\d+)/)[1]
+    }))
+    .get();
+
+  const rawSeasons = $('dd');
+
+  const seasons = rawSeasons.map(
+    element => element.find('a').text().match(/S\d+/g)
+  );
+
+  return series.map(
+    (s, index) => ({ title: s.title, id: s.id, seasons: seasons[index] })
+  );
+}
+
+export function parseCategories(categoriesHTML) {
+  const $ = cheerio.load(categoriesHTML);
+  const categoriesContainer = $('select#category optgroup');
+  let currentCategoryId = 0;
+
+  const categories = categoriesContainer.map(function getElements() {
+    currentCategoryId += 100;
+    const category = {
+      name: $(this).attr('label'),
+      id: `${currentCategoryId}`,
+      subcategories: []
+    };
+
+    $(this).find('option').each(function getSubcategory() {
+      const subcategory = {
+        id: $(this).attr('value'),
+        name: $(this).text()
+      };
+
+      return category.subcategories.push(subcategory);
+    });
+
+    return category;
+  });
+
+  return categories.get();
+}
---
+++ src/Torrent.js
@@ -0,0 +1,233 @@
+import querystring from 'querystring';
+import {
+  parsePage,
+  parseResults,
+  parseTorrentPage,
+  parseTvShow,
+  parseCategories
+} from './Parser';
+
+export const baseUrl = process.env.THEPIRATEBAY_DEFAULT_ENDPOINT || 'https://thepiratebay.org';
+
+export const defaultOrder = { orderBy: 'seeds', sortBy: 'desc' };
+
+const searchDefaults = {
+  category: '0',
+  page: '0',
+  filter: {
+    verified: false
+  },
+  orderBy: 'seeds',
+  sortBy: 'desc'
+};
+
+export const primaryCategoryNumbers = {
+  audio: 100,
+  video: 200,
+  applications: 300,
+  games: 400,
+  xxx: 500,
+  other: 600
+};
+
+/*
+ * opts:
+ *  category
+ *    0   - all
+ *    101 - 699
+ *  page
+ *    0 - 99
+ *  orderBy
+ *     1  - name desc
+ *     2  - name asc
+ *     3  - date desc
+ *     4  - date asc
+ *     5  - size desc
+ *     6  - size asc
+ *     7  - seeds desc
+ *     8  - seeds asc
+ *     9  - leeches desc
+ *     10 - leeches asc
+ */
+
+/**
+ * Take a orderBy object and convert it to its according number
+ *
+ * @example: { orderBy: 'leeches', sortBy: 'asc' }
+ * @example: { orderBy: 'name', sortBy: 'desc' }
+ */
+export function convertOrderByObject(orderByObject = defaultOrder) {
+  let searchNumber;
+
+  const options = [
+    ['name', 'desc'],
+    ['name', 'asc'],
+    ['date', 'desc'],
+    ['date', 'asc'],
+    ['size', 'desc'],
+    ['size', 'asc'],
+    ['seeds', 'desc'],
+    ['seeds', 'asc'],
+    ['leeches', 'desc'],
+    ['leeches', 'asc']
+  ];
+
+  for (const option of options) {
+    if (
+      option.includes(orderByObject.orderBy) &&
+      option.includes(orderByObject.sortBy)
+    ) {
+      searchNumber = options.indexOf(option) + 1;
+      break;
+    }
+  }
+
+  if (!searchNumber) throw Error("Can't find option");
+
+  return searchNumber;
+}
+
+/**
+ * Helper method for parsing page numbers
+ *
+ * @param  {number} pageNumber
+ * @return {string}
+ */
+function castNumberToString(pageNumber) {
+  if (typeof pageNumber === 'number') {
+    return String(pageNumber);
+  }
+
+  if (typeof pageNumber === 'string') {
+    return pageNumber;
+  }
+
+  if (
+    typeof pageNumber !== 'string' ||
+    typeof pageNumber !== 'number'
+  ) {
+    throw new Error('Unexpected page number type');
+  }
+}
+
+/**
+ * Determine the category number from an category name ('movies', 'audio', etc)
+ *
+ * @param  {number} || {string}
+ * @return {number}
+ */
+function resolveCategory(categoryParam) {
+  if (
+    typeof categoryParam === 'string' &&
+    categoryParam in primaryCategoryNumbers
+  ) {
+    return primaryCategoryNumbers[categoryParam];
+  }
+
+  return categoryParam;
+}
+
+function search(title = '*', opts = {}) {
+  const convertedCategory = resolveCategory(opts.category);
+
+  const castedOptions = {
+    ...opts,
+    page: opts.page ? castNumberToString(opts.page) : searchDefaults.page,
+    category: opts.category ? castNumberToString(convertedCategory) : searchDefaults.category,
+    orderby: opts.orderby ? castNumberToString(opts.orderby) : searchDefaults.orderby
+  };
+
+  const {
+    page,
+    category,
+    orderBy,
+    sortBy,
+    ...rest
+  } = { ...searchDefaults, ...castedOptions };
+
+  const orderingNumber = convertOrderByObject({ orderBy, sortBy });
+
+  const url = `${baseUrl}/s/?${querystring.stringify({
+    q: title,
+    category,
+    page,
+    orderby: orderingNumber
+  })}`;
+
+  return parsePage(url, parseResults, rest.filter);
+}
+
+function getTorrent(id) {
+  const url = (typeof id === 'number') || /^\d+$/.test(id)
+    ? `${baseUrl}/torrent/${id}`
+    : id.link || id;
+
+  return parsePage(url, parseTorrentPage);
+}
+
+function topTorrents(category = 'all') {
+  let castedCategory;
+
+  // Check if category is number and can be casted
+  if (parseInt(category, 10)) {
+    castedCategory = castNumberToString(category);
+  }
+
+  return parsePage(`${baseUrl}/top/${castedCategory || category}`, parseResults);
+}
+
+function recentTorrents() {
+  return parsePage(`${baseUrl}/recent`, parseResults);
+}
+
+function userTorrents(username, opts = {}) {
+  // This is the orderingNumber (1 - 10), not a orderBy param, like 'seeds', etc
+  let { orderby } = opts;
+
+  // Determine orderingNumber given orderBy and sortBy
+  if (opts.sortBy || opts.orderBy) {
+    orderby = convertOrderByObject({
+      sortBy: opts.sortBy || 'desc',
+      orderBy: opts.orderBy || 'seeds'
+    });
+  }
+
+  const query = `${baseUrl}/user/${username}/?${querystring.stringify({
+    page: opts.page ? castNumberToString(opts.page) : '0',
+    orderby: orderby || '99'
+  })}`;
+
+  return parsePage(query, parseResults);
+}
+
+/**
+ * @todo: url not longer returning results
+ * @broken
+ */
+// function tvShows() {
+//   return parsePage(`${baseUrl}'/tv/all`, parseTvShows);
+// }
+
+/**
+ * @todo: url not longer returning results
+ */
+function getTvShow(id) {
+  return parsePage(`${baseUrl}/tv/${id}`, parseTvShow);
+}
+
+function getCategories() {
+  return parsePage(`${baseUrl}/recent`, parseCategories);
+}
+
+export default {
+  search,
+  getTorrent,
+  topTorrents,
+  recentTorrents,
+  userTorrents,
+  getTvShow,
+  getCategories,
+  baseUrl,
+  searchDefaults,
+  defaultOrder
+};
