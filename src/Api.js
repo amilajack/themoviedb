c1267ceb3f8fa79b4dedb5ab8cb726e7590c8c98
Amila Welihinda <amilajack@gmail.com>
2016-10-30 14:23:19 -0700

    Renamed methods to endpoints, updated eslint rules

--- src/Api.js
+++ src/Api.js
@@ -1,2 +1,30 @@
 // @flow
-// import fetch from 'isomorphic-fetch';
+import fetch from 'isomorphic-fetch';
+import endpoints from './endpoints';
+
+
+export function setup(apiKey: string, baseUrl: string): Object {
+  const resolvedApiKey = apiKey || process.env.THE_MOVIE_DB_API_KEY || false;
+  const resolvedBaseUrl = baseUrl ||
+                          process.env.THE_MOVIE_DB_BASE_URL ||
+                          endpoints.base_url;
+
+  if (!resolvedApiKey) {
+    throw new Error('themoviedb setup: API key is a required paramater');
+  }
+
+  return {
+    API_KEY: resolvedApiKey,
+    BASE_URL: resolvedBaseUrl
+  };
+}
+
+export function requestToken(config: Object): Promise<string> {
+  return fetch(
+    `${config.base_url}${endpoints.authentication.requestToken}`
+  )
+  .then(res => res.json())
+  .then(res => res.body);
+}
+
+export default {};
