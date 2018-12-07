cf9a4db39e5210225130242f688ce5ac365e8a17
Amila Welihinda <amilajack@gmail.com>
2018-12-06 23:19:03 -0800

    initial working infra

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv';
 import { setup } from '../src';
+
+dotenv.config();
 
 describe('basic', () => {
   it('should setup()', () => {
-    setup();
+    setup(process.env.THE_MOVIE_DB_API_KEY);
   });
 });
